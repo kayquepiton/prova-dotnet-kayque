c704b8551abe5e3cd4badd55f3b6c7d1d54df94d
kayquepiton <kayquepiton@gmail.com>
2023-11-30 16:11:19 -0300

    prova - relatorios

--- Program.cs
+++ Program.cs
@@ -16,6 +16,43 @@ class GerenciadorEscritorioAdvogacia{
             new Cliente("Cliente2", new DateTime(1995, 8, 25), "55566677788", EstadoCivil.Casado, "Médico")
         };
 
+        // Relatório 1: Advogados com idade entre dois valores
+        var advogadosPorIdade = advogados.Where(a => a.Idade >= 30 && a.Idade <= 40);
+        Console.WriteLine("Advogados com idade entre 30 e 40 anos:");
+        foreach (var adv in advogadosPorIdade){
+            Console.WriteLine($"Nome: {adv.Nome}, Idade: {adv.Idade}");
+        }
+
+        // Relatório 2: Clientes com idade entre dois valores
+        var clientesPorIdade = clientes.Where(c => c.Idade >= 25 && c.Idade <= 35);
+        Console.WriteLine("\nClientes com idade entre 25 e 35 anos:");
+        foreach (var cli in clientesPorIdade){
+            Console.WriteLine($"Nome: {cli.Nome}, Idade: {cli.Idade}");
+        }
+
+        // Relatório 3: Clientes com estado civil informado pelo usuário
+        EstadoCivil estadoCivilInformado = EstadoCivil.Casado; // Substitua pelo valor informado pelo usuário
+        var clientesPorEstadoCivil = clientes.Where(c => c.EstadoCivil == estadoCivilInformado);
+        Console.WriteLine($"\nClientes com estado civil {estadoCivilInformado}:");
+        foreach (var cli in clientesPorEstadoCivil){
+            Console.WriteLine($"Nome: {cli.Nome}, Estado Civil: {cli.EstadoCivil}");
+        }
+
+        // Relatório 4: Clientes em ordem alfabética
+        var clientesOrdenados = clientes.OrderBy(c => c.Nome);
+        Console.WriteLine("\nClientes em ordem alfabética:");
+        foreach (var cli in clientesOrdenados){
+            Console.WriteLine($"Nome: {cli.Nome}");
+        }
+
+        // Relatório 5: Clientes cuja profissão contenha texto informado pelo usuário
+        string textoProfissao = "Médico"; // Substitua pelo texto informado pelo usuário
+        var clientesPorProfissao = clientes.Where(c => c.Profissao.Contains(textoProfissao));
+        Console.WriteLine($"\nClientes com profissão contendo '{textoProfissao}':");
+        foreach (var cli in clientesPorProfissao){
+            Console.WriteLine($"Nome: {cli.Nome}, Profissão: {cli.Profissao}");
+        }
+
         
     }
 }
