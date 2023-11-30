b6eca41d8743354e1138b7ed4a94297e8733d458
kayquepiton <kayquepiton@gmail.com>
2023-11-30 15:46:49 -0300

    prova - classes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Enum para o estado civil
+enum EstadoCivil{
+    Solteiro,
+    Casado
+}
+
+// Interface para representar Advogado e Cliente
+interface IPessoa{
+    string Nome { get; }
+    DateTime DataNascimento { get; }
+}
+
+// Classe base para Advogado e Cliente
+abstract class Pessoa : IPessoa{
+    public string Nome { get; }
+    public DateTime DataNascimento { get; }
+
+    protected Pessoa(string nome, DateTime dataNascimento){
+        Nome = nome;
+        DataNascimento = dataNascimento;
+    }
+
+    public int Idade => DateTime.Now.Year - DataNascimento.Year;
+}
+
+// Classe Advogado
+class Advogado : Pessoa{
+    public string CPF { get; }
+    public string CNA { get; }
+
+    public Advogado(string nome, DateTime dataNascimento, string cpf, string cna)
+        : base(nome, dataNascimento)
+    {
+        // Validar CPF com 11 dígitos
+        if (cpf.Length != 11 || !cpf.All(char.IsDigit)){
+            throw new ArgumentException("CPF inválido");
+        }
+
+        CPF = cpf;
+        CNA = cna;
+    }
+}
